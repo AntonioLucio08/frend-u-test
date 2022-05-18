e12bde401bcffd6c26788645029a351156735e0c
Antonio Lucio <jalg0822@gmail.com>
2022-05-17 21:22:55 -0500

    forguet before commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Button,TextField} from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <div>
         <nav className='navbar-login'> Frend-U</nav>
-        <di className='navbar-items'>
+        <div className='navbar-items'>
           <h1 className='title-items'>AMINISTRACION</h1>
-          <div>
-            <ul>
-              <li>User Name</li>
-              <li>E-mail</li>
-              <li>Password</li>
-            </ul>
-            
-          </div>
-        </di>
-      </div>
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+        />
+        </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/userName.js
@@ -0,0 +1,10 @@
+
+import {TextField} from '@mui/material/TextField';
+
+function userName (){
+    return(
+
+        <TextField label="Outlined secondary" color="secondary" focused />
+    )
+  }
+  export default userName;
